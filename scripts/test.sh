08533d237f282dc90fe6a34d4a790d27e3351888
Avishay Bar <avishay.il@gmail.com>
2020-12-17 18:13:48 +0200

    chore: improvements

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ regions_array=(us-east-1 us-east-2 us-west-1 us-west-2 ca-central-1 eu-west-1 eu
 for i in ${regions_array[@]}
 do
   export CDK_DEFAULT_REGION=$i
+  export CDK_DEFAULT_ACCOUNT="746046487306"
   export AWS_REGION=$i
   pytest .
 done
